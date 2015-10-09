2bf8b9030d73dc80049464588b49da25887f9e1b
yukato <manabi0215@gmail.com>
2015-10-09 12:49:12 +0000

    relationship changed

--- db/migrate/20150913085437_create_relationships.rb
+++ db/migrate/20150913085437_create_relationships.rb
@@ -1,8 +1,8 @@
 class CreateRelationships < ActiveRecord::Migration
   def change
     create_table :relationships do |t|
-      t.references :follower, index: true, foreign_key: true
-      t.references :followed, index: true, foreign_key: true
+      t.integer :follower_id, index: true, foreign_key: true
+      t.integer :followed_id, index: true, foreign_key: true
 
       t.timestamps null: false
       t.index [:follower_id, :followed_id], unique: true
